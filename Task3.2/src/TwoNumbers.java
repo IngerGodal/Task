327359db6c91968040738ee205240074b8f3205a
Inger <jordi1@mail.ru>
2017-10-28 13:02:19 +0400

    "Task3.2 solution For"

--- Task3.2/src/TwoNumbers.java
+++ Task3.2/src/TwoNumbers.java
@@ -13,9 +13,11 @@ public class TwoNumbers {
 
         System.out.println("_______Решение задачи_______");
         solutionWhile(number1, number2);
+        System.out.println("_______Решение задачи через цикл For_______");
+        temp.solutionFor(number1, number2);
     }
 
-    static void solutionWhile(int number1, int number2) {
+    public void solutionWhile(int number1, int number2) {
         int limitOfTry;
         int resultMultiplication;
         int countTry;
@@ -38,6 +40,36 @@ public class TwoNumbers {
             }
 
             countTry++;
+        }
+
+        if (flag == true) {
+            System.out.print("Цель достигнута! Количество попыток : " + "[ " + countTry + " ]");
+        } else {
+            System.out.print("Цель не достигнута, попытки исчерпаны...");
+        }
+    }
+
+    public void solutionFor(int number1, int number2) {
+        int limitOfTry;
+        int resultMultiplication;
+        int countTry;
+        int tmp;
+        boolean flag;
+
+        countTry = 1;
+        limitOfTry = 10;
+        tmp = number1;
+        flag = false;
+
+        resultMultiplication = number1 * number2;
+
+        for (int i = 1; i <= limitOfTry; i++) {
+            tmp += number2;
+
+            if (tmp > resultMultiplication) {
+                flag = true;
+                break;
+            }
         }
 
         if (flag == true) {
