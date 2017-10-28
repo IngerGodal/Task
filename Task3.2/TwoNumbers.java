40b0f907e2d4762065cdf74ab3044107d0e8e47d
Inger <jordi1@mail.ru>
2017-10-28 13:49:18 +0400

    Task3.2 - solution DoWhile

--- Task3.2/TwoNumbers.java
+++ Task3.2/TwoNumbers.java
@@ -19,6 +19,9 @@ public class TwoNumbers {
 		
 		System.out.println("_______Решение задачи через цикл For_______");
         temp.solutionFor(number1, number2);
+		
+		System.out.println("_______Решение задачи через цикл DoWhile_______");
+        temp.solutionDoWhile(number1, number2);
 	}
 	
 	public int solutionWhile(int number1, int number2) {
@@ -75,6 +78,36 @@ public class TwoNumbers {
                 break;
             }
         }
+
+        if (flag == true) {
+            System.out.println("Цель достигнута! Количество попыток : " + "[ " + countTry + " ]");
+        } else {
+            System.out.println("Цель не достигнута, попытки исчерпаны...");
+        }
+    }
+	
+	public void solutionDoWhile(int number1, int number2) {
+        int limitOfTry;
+        int resultMultiplication;
+        int countTry;
+        int tmp;
+        boolean flag;
+
+        countTry = 1;
+        limitOfTry = 10;
+        tmp = number1;
+        flag = false;
+
+        resultMultiplication = number1 * number2;
+
+        do {
+            tmp += number2;
+
+            if (tmp > resultMultiplication) {
+                flag = true;
+                break;
+            }
+        } while (++countTry <= limitOfTry);
 
         if (flag == true) {
             System.out.println("Цель достигнута! Количество попыток : " + "[ " + countTry + " ]");
